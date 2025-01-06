9230ef28d5c477f96d83474372b3e34f95cbb169
Shravani <talwarshravni@gmail.com>
2025-01-06 19:34:50 +0530

    Circle is added

---
+++ circle.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(
+{
+    double radius;
+    cout << "Enter the radius of the circle: ";
+    cin >> radius;
+    cout << "The area of the circle is " << 3.14159 * radius * radius << endl;
+    cout << "The circumference of the circle is " << 2 * 3.14159 * radius << endl;
+    return 0;
+} // main(
+)
